89238260a0b124825ac484ab318239a931cde23d
Balaji <63382443+BALAJI-ng@users.noreply.github.com>
2025-06-18 22:50:23 -0700

    JIRA-1-Projecr FE-BE Commit

---
+++ BE_SpringBoot/kindergarden-api/src/test/java/com/example/restApi/kindergarden_api/KindergardenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restApi.kindergarden_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergardenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
